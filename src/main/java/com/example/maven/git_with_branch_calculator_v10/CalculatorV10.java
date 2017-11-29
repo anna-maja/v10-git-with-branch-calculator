62a00b8440c934796f53977ba3c875dfd3955777
Anna-Maja Lithner <am_lithner@hotmail.com>
2017-11-29 12:03:44 +0100

    Added method division in CalculatorV10. Created test methods for division, with two numbers and with second number zero.

--- src/main/java/com/example/maven/git_with_branch_calculator_v10/CalculatorV10.java
+++ src/main/java/com/example/maven/git_with_branch_calculator_v10/CalculatorV10.java
@@ -11,9 +11,24 @@ public class CalculatorV10 {
 
 		return firstNumber - secondNumber;
 	}
-	
+
 	public int multiply(int firstNumber, int secondNumber) {
 
 		return firstNumber * secondNumber;
 	}
+
+	public double division(int firstNumber, int secondNumber) {
+
+		double result = 0;
+		
+		if (secondNumber != 0) {
+			result = firstNumber / secondNumber;
+		} else {
+			System.out.println("\nDivision by zero is not possible and will give the fake result -0.123456789");
+			result = -0.123456789;
+		}
+		return result;
+
+	}
+
 }
