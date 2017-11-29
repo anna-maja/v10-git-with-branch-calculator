8feefc479e7ce0253dd803bbedaca03ccb2e8e90
Anna-Maja Lithner <am_lithner@hotmail.com>
2017-11-29 10:13:30 +0100

    Added method subtract in CalculatorV10.

--- src/main/java/com/example/maven/git_with_branch_calculator_v10/CalculatorV10.java
+++ src/main/java/com/example/maven/git_with_branch_calculator_v10/CalculatorV10.java
@@ -2,8 +2,13 @@ package com.example.maven.git_with_branch_calculator_v10;
 
 public class CalculatorV10 {
 
-	public int add (int firstNumber, int secondNumber) {
-		
-		return firstNumber + secondNumber; 
+	public int add(int firstNumber, int secondNumber) {
+
+		return firstNumber + secondNumber;
+	}
+
+	public int subtract(int firstNumber, int secondNumber) {
+
+		return firstNumber - secondNumber;
 	}
 }
